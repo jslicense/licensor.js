618cdb24f7efc1ee5688ea71d7ff9249579a29b4
Kyle E. Mitchell <kyle@kemitchell.com>
2016-02-14 01:50:00 -0800

    Fix Apache 2.0 test to expect blank line after header

--- tests/apache-2.0/test.js
+++ tests/apache-2.0/test.js
@@ -47,7 +47,7 @@ tap.equal(
       '// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.',
       '// See the License for the specific language governing permissions and',
       '// limitations under the License.' ]
-      .join('\n') + '\n' +
+      .join('\n') + '\n\n' +
       indexBefore ))
 
 tap.equal(
